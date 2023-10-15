d39ac4ffaa9db17ae50216e2acf1a03677e5f7ad
MJun111 <kmj0568@gmail.com>
2023-10-16 00:56:34 +0900

    [FEAT] add Board CRUD in Controller

--- src/main/java/com/bitbox/board/controller/BoardController.java
+++ src/main/java/com/bitbox/board/controller/BoardController.java
@@ -1,5 +1,8 @@
 package com.bitbox.board.controller;
 
+import com.bitbox.board.dto.request.BoardModifyRequestDto;
+import com.bitbox.board.dto.request.BoardRegisterRequestDto;
+import com.bitbox.board.dto.response.BoardDetailResponseDto;
 import com.bitbox.board.dto.response.BoardListResponseDto;
 import com.bitbox.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -7,8 +10,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,19 +29,55 @@ public class BoardController {
 
   private final BoardService boardService;
 
-  @GetMapping
-  public String test() {
-    return "Test";
-  }
-
   @GetMapping("/{boardType}")
   public ResponseEntity<BoardListResponseDto> getBoardList(
       @PathVariable("boardType") String boardType,
       @RequestParam(value = "category") Long categoryId,
-      @PageableDefault(size = 5) Pageable pageable) {
+      @PageableDefault(size = 5, sort = "created_at,desc") Pageable pageable
+  ) throws Exception {
 
-
-
-    return ResponseEntity.ok(null);
+    return ResponseEntity.ok(boardService.getBoardList(pageable, categoryId));
   }
+
+  @GetMapping("{boardType}/detail/{boardId}")
+  public ResponseEntity<BoardDetailResponseDto> getBoardDetail(
+      @PathVariable("boardType") String boardType,
+      @PathVariable("boardId") Long boardId,
+      @RequestHeader("memberId") String memberId,
+      @RequestHeader("authority") String authority
+  ) throws Exception {
+
+    return ResponseEntity.ok(boardService.getBoardDetail(boardId, memberId, authority));
+  }
+
+  @PostMapping("/{boardType}")
+  public ResponseEntity<Boolean> registerBoard(
+      @RequestBody BoardRegisterRequestDto request,
+      @RequestHeader("memberId") String memberId,
+      @RequestHeader("memberName") String memeberName
+  ) throws Exception {
+    return ResponseEntity.ok(boardService.registerBoard(request, memberId, memeberName));
+  }
+
+  @PutMapping("/{boardType}")
+  public ResponseEntity<Boolean> modifyBoard(
+      @RequestBody BoardModifyRequestDto request,
+      @RequestHeader("memberId") String memberId
+  ) throws Exception {
+    return ResponseEntity.ok(boardService.modifyBoard(request, memberId));
+  }
+
+  @DeleteMapping("/{boardType}/{boardId}")
+  public ResponseEntity<Boolean> removeBoard(
+      @PathVariable("boardType") String boardType,
+      @PathVariable("boardId") Long boardId,
+      @RequestHeader("memberId") String memberId,
+      @RequestHeader("authority") String authority
+  ) throws Exception {
+    return ResponseEntity.ok(boardService.removeBoard(boardId, memberId, authority));
+  }
+
+  // Todo 사용자 게시글, 댓글 조회
+  // Todo 댓글 작성, 수정
+
 }
