2fc8efa149b9ffa1e8d5658e1fe29a36feaa1fdf
MJun111 <kmj0568@gmail.com>
2023-10-20 16:25:12 +0900

    [FEAT] fix logic in get board list

--- src/main/java/com/bitbox/board/repository/CategoryRepository.java
+++ src/main/java/com/bitbox/board/repository/CategoryRepository.java
@@ -1,10 +1,12 @@
 package com.bitbox.board.repository;
 
 import com.bitbox.board.entity.Category;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
   Optional<Category> findByCategoryName(String categoryName);
+  List<Category> findByMasterCategory_Id(Long id);
 }
